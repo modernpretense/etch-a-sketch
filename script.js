22008e9c8c38ecbd2e750e722769bae1e51a99cf
Morten <7154135+modernpretense@users.noreply.github.com>
2023-02-25 19:33:45 +0100

    Make square div logic added

--- script.js
+++ script.js
@@ -2,4 +2,13 @@
 
 const content = document.querySelector('#content');
 
-const gridsquare = document.createElement('div').classList.add('gridsquare');
+//const gridsquare = document.createElement('div').classList.add('gridsquare');
+
+function makeSquare() {
+    const make = document.createElement('div');
+    make.classList.add('gridsquare');
+
+    content.appendChild(make);
+};
+
+makeSquare();
