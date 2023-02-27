ea527c13c3e5adb514ae3a32688b3c90eae22d6c
Morten <7154135+modernpretense@users.noreply.github.com>
2023-02-27 12:52:19 +0100

    hover logic implemented

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // Etch-a-Sketch Javascript
 
+// Grid generation
+
 const content = document.querySelector('#content');
 
 let gridsize;
@@ -12,20 +14,30 @@ function makeLine(num) {
     for (let i = 0; i < gridsize; i++) { 
         makeSquare(line.id);
     };
-};
+}
 
 function makeSquare(lineinput) {
     const make = document.createElement('div');
     make.classList.add('gridsquare');
     const lineid = document.getElementById(lineinput);
     lineid.appendChild(make);
-};
+}
 
 function makeGrid(number) {
     gridsize = number;
     for (let i = 0; i < number; i++) {
         makeLine(i);
     }
-};
+}
 
 makeGrid(16);
+
+// Coloring logic
+
+const pixels = document.querySelectorAll('.gridsquare');
+
+pixels.forEach((pixel) => {
+    pixel.addEventListener('mouseenter', () => {
+        pixel.classList.add('colored');
+    });
+}); 
